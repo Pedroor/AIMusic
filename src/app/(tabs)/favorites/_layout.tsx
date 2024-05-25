f0591cc5d9198304d0017243dea8759754f2044f
Pedro Arthur <pedroarthur06@alu.ufc.br>
2024-05-25 11:03:04 -0300

    feat: add favorites page

--- src/app/(tabs)/favorites/_layout.tsx
+++ src/app/(tabs)/favorites/_layout.tsx
@@ -1,19 +1,22 @@
-import { StackScreenWithSearchBar } from "@/constants/layout";
-import { defaultStyles } from "@/styles";
-import { Stack } from "expo-router";
-import { View } from "react-native";
+import { StackScreenWithSearchBar } from '@/constants/layout'
+import { defaultStyles } from '@/styles'
+import { Stack } from 'expo-router'
+import { View } from 'react-native'
 
 const FavoritesScreenLayout = () => {
-  return (
-    <View style={defaultStyles.container}>
-      <Stack>
-        <Stack.Screen
-          name="index"
-          options={{ ...StackScreenWithSearchBar, headerTitle: "Favorites" }}
-        />
-      </Stack>
-    </View>
-  );
-};
+	return (
+		<View style={defaultStyles.container}>
+			<Stack>
+				<Stack.Screen
+					name="index"
+					options={{
+						...StackScreenWithSearchBar,
+						headerTitle: 'Favorites',
+					}}
+				/>
+			</Stack>
+		</View>
+	)
+}
 
-export default FavoritesScreenLayout;
+export default FavoritesScreenLayout
